6045b5cfcbaa73844dea6f3d9fa82256217a1639
Jhonnyllucius <lucianofloripa@outlook.com>
2025-11-21 23:36:18 -0300

    feat: implementar suporte a localização com Bloc e ajustar tema da aplicação

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:base_project/src/core/core.dart';
+import 'package:base_project/src/modules/splash/splash_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF232F69))),
-      home: const PaymentsTransactionsPage(),
+    return BlocProvider<LocaleBloc>(
+      create:
+          (context) => getIt<LocaleBloc>()..add(const LoadSavedLocaleEvent()),
+      child: BlocBuilder<LocaleBloc, LocaleState>(
+        builder: (context, state) {
+          return MaterialApp(
+            title: 'Paymobi Payments',
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            locale: state is LocaleLoaded ? state.locale : const Locale('pt'),
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [Locale('en'), Locale('pt')],
+            home: const SplashPage(),
+          );
+        },
+      ),
     );
-  }
-}
-
-class PaymentsTransactionsPage extends StatelessWidget {
-  const PaymentsTransactionsPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Container(color: Color(0xFF232F69)));
   }
 }
