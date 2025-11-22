7f0ee57c5bca00297e50c7558665e2aee374264e
Jhonnyllucius <lucianofloripa@outlook.com>
2025-11-21 23:36:01 -0300

    refactor: alterar a função main para ser assíncrona e inicializar dependências

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:base_project/src/app_widget.dart';
+import 'package:base_project/src/core/core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa as dependências
+  await setupDependencies();
+
   runApp(const AppWidget());
 }
