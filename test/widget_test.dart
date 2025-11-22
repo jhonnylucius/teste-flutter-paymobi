162e8c72e8d25a523b454e5c0bf7ba9e77ef76d3
Jhonnyllucius <lucianofloripa@outlook.com>
2025-11-21 23:42:38 -0300

    feat: adicionar estrutura básica de testes para widgets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Widget Tests', () {
+    test('placeholder test', () {
+      expect(true, true);
+    });
+  });
+}
