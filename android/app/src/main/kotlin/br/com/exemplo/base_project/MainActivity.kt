f6cf05608874c32a7bd10291ece6194c7e198b83
Jhonnyllucius <lucianofloripa@outlook.com>
2025-11-22 00:09:30 -0300

    feat: implementar método onCreate na MainActivity para habilitar edge-to-edge

--- android/app/src/main/kotlin/br/com/exemplo/base_project/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/exemplo/base_project/MainActivity.kt
@@ -1,5 +1,17 @@
 package br.com.exemplo.base_project
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Habilita edge-to-edge (Android 15+)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+    }
+}
