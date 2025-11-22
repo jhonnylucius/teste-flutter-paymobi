599fb9ae5ddf241a548f2231139af40bd1184f86
Jhonnyllucius <lucianofloripa@outlook.com>
2025-11-21 23:39:28 -0300

    refactor: melhorar a legibilidade do código ao formatar a função getPaymentsInfo

--- lib/src/modules/payments/infra/datasource/payments_datasource_impl.dart
+++ lib/src/modules/payments/infra/datasource/payments_datasource_impl.dart
@@ -9,7 +9,9 @@ class PaymentsDatasourceImpl implements PaymentsDataSource {
   @override
   Future<PaymentsInfoEntity> getPaymentsInfo() async {
     try {
-      final response = await Future.delayed(Duration(milliseconds: 1500)).then((_) {
+      final response = await Future.delayed(Duration(milliseconds: 1500)).then((
+        _,
+      ) {
         // INFO: use mockEmptyJson or mockPaymentsJson
         return mockPaymentsJson; /* mockEmptyJson */
       });
